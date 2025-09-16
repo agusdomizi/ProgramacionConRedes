f9b7496c685ca26b27afc6ea33c5c873f1d84476
Luis Agustin Domizi Rius <79983430+agusdomizi@users.noreply.github.com>
2025-09-16 17:55:20 -0300

    Add files via upload

---
+++ prof/Especiales/JSON/Unidades_de_medida.js
@@ -0,0 +1,9 @@
+
+var UnidadesDeMedida = [
+	{ cod: "UN", descripcion: "Unidad" },
+	{ cod: "HS", descripcion: "Hora de servicio" },
+	{ cod: "PQ", descripcion: "Paquete" },
+    { cod: "MT", descripcion: "Metro" },
+    { cod: "KG", descripcion: "Kilogramo" },
+    { cod: "LT", descripcion: "Litro" },
+];
---
+++ prof/Especiales/JSON/factura_de_compra.js
@@ -0,0 +1,173 @@
+[
+    {
+  "NroFactura": "FAC-0001",
+      "cod-prod": "PRD-001",
+      "Descripcion-producto-servicio": "Laptop Dell Inspiron 15",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 2,
+      "PrecioUnit": 850.00,
+      "TotalRenglon": 1700.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0001",
+      "cod-prod": "PRD-002",
+      "Descripcion-producto-servicio": "Mouse inalámbrico Logitech",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 5,
+      "PrecioUnit": 20.00,
+      "TotalRenglon": 100.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0001",
+      "cod-prod": "PRD-003",
+      "Descripcion-producto-servicio": "Monitor Samsung 24'' LED",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 3,
+      "PrecioUnit": 180.00,
+      "TotalRenglon": 540.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0002",
+      "cod-prod": "PRD-017",
+      "Descripcion-producto-servicio": "Servicio de instalación de equipos",
+      "Unidad-medida": { "cod-uni": "HS", "Descripcion": "Hora de servicio" },
+      "Cantidad": 4,
+      "PrecioUnit": 50.00,
+      "TotalRenglon": 200.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0002",
+      "cod-prod": "PRD-004",
+      "Descripcion-producto-servicio": "Impresora HP LaserJet",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 2,
+      "PrecioUnit": 300.00,
+      "TotalRenglon": 600.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0002",
+      "cod-prod": "PRD-005",
+      "Descripcion-producto-servicio": "Resma de papel A4 500 hojas",
+      "Unidad-medida": { "cod-uni": "PQ", "Descripcion": "Paquete" },
+      "Cantidad": 10,
+      "PrecioUnit": 6.50,
+      "TotalRenglon": 65.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0003",
+      "cod-prod": "PRD-006",
+      "Descripcion-producto-servicio": "Proyector Epson XGA",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 1,
+      "PrecioUnit": 750.00,
+      "TotalRenglon": 750.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0003",
+      "cod-prod": "PRD-007",
+      "Descripcion-producto-servicio": "Router Cisco",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 2,
+      "PrecioUnit": 400.00,
+      "TotalRenglon": 800.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0004",
+      "cod-prod": "PRD-008",
+      "Descripcion-producto-servicio": "Escritorio de oficina",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 4,
+      "PrecioUnit": 150.00,
+      "TotalRenglon": 600.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0004",
+      "cod-prod": "PRD-009",
+      "Descripcion-producto-servicio": "Silla ergonómica ejecutiva",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 6,
+      "PrecioUnit": 90.00,
+      "TotalRenglon": 540.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0004",
+      "cod-prod": "PRD-010",
+      "Descripcion-producto-servicio": "Cámara de seguridad",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 5,
+      "PrecioUnit": 110.00,
+      "TotalRenglon": 550.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0005",
+      "cod-prod": "PRD-016",
+      "Descripcion-producto-servicio": "Capacitación en ofimática",
+      "Unidad-medida": { "cod-uni": "HS", "Descripcion": "Hora de capacitación" },
+      "Cantidad": 8,
+      "PrecioUnit": 40.00,
+      "TotalRenglon": 320.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0005",
+      "cod-prod": "PRD-011",
+      "Descripcion-producto-servicio": "Servidor Dell PowerEdge",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 1,
+      "PrecioUnit": 2500.00,
+      "TotalRenglon": 2500.00,
+      "PdfComprobante": ""
+    },
+    {
+  "NroFactura": "FAC-0005",
+      "cod-prod": "PRD-012",
+      "Descripcion-producto-servicio": "Switch de red Netgear 24 puertos",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 2,
+      "PrecioUnit": 180.00,
+      "TotalRenglon": 360.00,
+      "PdfComprobante": ""
+    },
+    {
+      "NroFactura": "FAC-0006",
+      "cod-prod": "PRD-013",
+      "Descripcion-producto-servicio": "Tablet Apple iPad 10.2''",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 3,
+      "PrecioUnit": 329.00,
+      "TotalRenglon": 987.00,
+      "PdfComprobante": ""
+    },
+    {
+      "NroFactura": "FAC-0006",
+      "cod-prod": "PRD-015",
+      "Descripcion-producto-servicio": "Soporte técnico remoto mensual",
+      "Unidad-medida": { "cod-uni": "CN", "Descripcion": "Contrato" },
+      "Cantidad": 1,
+      "PrecioUnit": 150.00,
+      "TotalRenglon": 150.00,
+      "PdfComprobante": ""
+    },
+    {
+      "NroFactura": "FAC-0006",
+      "cod-prod": "PRD-014",
+      "Descripcion-producto-servicio": "Cargador portátil Xiaomi 10000mAh",
+      "Unidad-medida": { "cod-uni": "UN", "Descripcion": "Unidad" },
+      "Cantidad": 4,
+      "PrecioUnit": 25.00,
+      "TotalRenglon": 100.00,
+      "PdfComprobante": ""
+    }
+  ]
+  
