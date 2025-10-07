bfb53b7cfa0ba6c133906eaea264946b6f098631
Luis Agustin Domizi Rius <79983430+agusdomizi@users.noreply.github.com>
2025-10-07 12:51:18 -0300

    Add files via upload

---
+++ prof/Php/phpBase/index.php
@@ -0,0 +1,7 @@
+<h2>Ejercicios de Php (Texto escrito fuera del marcado php)</h2>
+<br>
+<?php
+    echo "<h3>Hola mundo desde php</h3>";
+    echo "<p>Este es mi primer script en php</p>";
+    echo "<br>";
+?>
